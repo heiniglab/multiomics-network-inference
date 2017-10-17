d443ef625a1dfd3c98cdf25f6690679b2638199d
Johann Hawe <johann.hawe@helmholtz-muenchen.de>
2017-10-17 17:41:16 +0200

    Fixed issue in latest version of BDgraph

--- bdgraph/BDgraph_2.40/src/omp_set_num_cores.cpp
+++ bdgraph/BDgraph_2.40/src/omp_set_num_cores.cpp
@@ -1,13 +1,18 @@
+#include <sstream>
+#include <string>        // std::string, std::to_string
+#include <vector>        // for using vector
+#include <limits>        // for std::numeric_limits<double>::max()
+#include <Rmath.h>
 #include <Rconfig.h>
-#ifdef SUPPORT_OPENMP
 #include <omp.h>
-#endif
+#include <R.h>
 
 extern "C" {
 void omp_set_num_cores( int *cores ) 
 {
-	#ifdef SUPPORT_OPENMP
-		omp_set_num_threads( *cores );
-	#endif
+   // we removed the portion which tests for the 
+   // SUPPORT_OPENMP definition, since this seemed not to work
+   // for us despite specifiying -fopenm for the R compiler options
+   omp_set_num_threads( *cores );
 }
 }

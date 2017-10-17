ca123c44eccdf499c3a5b97a5eaa99382bf26402
Johann Hawe <johann.hawe@helmholtz-muenchen.de>
2017-10-17 17:41:54 +0200

    Added batch script for ggm validation

---
+++ R/validate-ggm_batch.R
@@ -0,0 +1,10 @@
+# submits the ggm calculation for each availablie sentinel to the server
+# parses the sentinels from the available data files
+load("results/data.processed.RData")
+
+for(sentinel in names(data)){
+  cmd <- paste0("qsub -cwd -V -q long_fed25 -pe smp 1 -hard -l job_mem=4G -b y ")
+  cmd <- paste0(cmd, "-N ", sentinel," -o ", sentinel, ".out -e ", sentinel, ".out ")
+  cmd <- paste0(cmd, "Rscript 2-validate-ggm.R ", sentinel)
+  system(cmd)
+}

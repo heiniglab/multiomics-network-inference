39e95f8fdd469c94b8f7c8d898d209278c72d34c
Johann Hawe <johann.hawe@helmholtz-muenchen.de>
2017-10-17 17:39:25 +0200

    Minor change in lib script

--- R/lib.R
+++ R/lib.R
@@ -101,7 +101,7 @@ graph.from.fit <- function(ggm.fit, ranges){
   library(igraph)
   
   # get the graph instance from the ggm fit
-  cutoff <- 0.8
+  cutoff <- 0.9
   g.adj <- BDgraph::select(ggm.fit, cut = cutoff)
   g <- as_graphnel(graph.adjacency(g.adj, mode="undirected", diag=F));
   gn <- nodes(g)
@@ -391,9 +391,9 @@ get.nearby.ranges <- function(query, subject) {
 
   nearby <- function(q,s){
     # get preceding, following and ovberlapping instances of any range in query within subject ranges
-    pre <- precede(q, s, select="all")
-    fol <- follow(q, s, select="all")
-    ove <- findOverlaps(q, s, select="all")
+    pre <- precede(q, s, select="all", ignore.strand=T)
+    fol <- follow(q, s, select="all", ignore.strand=T)
+    ove <- findOverlaps(q, s, select="all", ignore.strand=T)
     # combine hits
     h <- unique(c(subjectHits(pre), subjectHits(fol), subjectHits(ove)))
 

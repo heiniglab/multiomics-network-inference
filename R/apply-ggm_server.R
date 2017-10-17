ed3769287f11186b0b015bedad5c677117659a84
Johann Hawe <johann.hawe@helmholtz-muenchen.de>
2017-10-17 17:40:21 +0200

    Adjusted batch/server scripts for ggm application

--- R/apply-ggm_server.R
+++ R/apply-ggm_server.R
@@ -1,13 +1,12 @@
 # this script calls for each of the available sentinels (parsed from the data)
 # the ggm modelling scripts.
 library(rmarkdown)
-snp <- commandArgs(trailingOnly = T)
-
-#snp <- "rs9859077"
-snp <- "rs7783715"
+args <- commandArgs(trailingOnly = T)
+snp <- args[1]
+cores <- args[2]
 
 # make sure output dir exists
 dir.create("results/reports", recursive = F, showWarnings = F)
 render("1-apply-ggm.Rmd", 
-       params=list(SNP=snp), 
+       params=list(SNP=snp, cores=cores), 
        output_file = paste0("results/reports/apply-ggm-", snp, ".html"))

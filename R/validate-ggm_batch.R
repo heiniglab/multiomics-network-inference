43020b434da981579bac8efb2c60d9219ee2b5cd
Johann Hawe <johann.hawe@helmholtz-muenchen.de>
2017-10-19 11:15:13 +0200

    Fixed bug concerning validation log files.

--- R/validate-ggm_batch.R
+++ R/validate-ggm_batch.R
@@ -4,7 +4,7 @@ load("results/data.processed.RData")
 
 for(sentinel in names(data)){
   cmd <- paste0("qsub -cwd -V -q long_fed25 -pe smp 1 -hard -l job_mem=4G -b y ")
-  cmd <- paste0(cmd, "-N ", sentinel," -o ", sentinel, ".validation..out -e ", sentinel, ".validation.out ")
+  cmd <- paste0(cmd, "-N ", sentinel," -o ", sentinel, ".validation.out -e ", sentinel, ".validation.out ")
   cmd <- paste0(cmd, "Rscript 2-validate-ggm.R ", sentinel)
   system(cmd)
 }

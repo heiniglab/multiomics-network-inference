4441b4b40067bdaf29cdd27e6b475379fad39f7a
Johann Hawe <johann.hawe@helmholtz-muenchen.de>
2017-11-06 18:19:00 +0100

    Fixed output paths for validation batch script

--- R/validate-ggm_batch.R
+++ R/validate-ggm_batch.R
@@ -2,9 +2,11 @@
 # parses the sentinels from the available data files
 load("results/current/data.processed.RData")
 
-for(sentinel in names(data)){
+for(sentinel in c("rs3809627","rs17850402","rs12412214","rs11793438")) { #names(data)){
   cmd <- paste0("qsub -cwd -V -q long_fed25 -pe smp 1 -hard -l job_mem=4G -b y ")
-  cmd <- paste0(cmd, "-N ", sentinel," -o ", sentinel, ".validation.out -e ", sentinel, ".validation.out ")
+  cmd <- paste0(cmd, "-N ", sentinel, " ",
+                "-o results/current/", sentinel, ".validation.out ", 
+                "-e results/current/", sentinel, ".validation.out ")
   cmd <- paste0(cmd, "Rscript 2-validate-ggm.R ", sentinel)
   system(cmd)
 }

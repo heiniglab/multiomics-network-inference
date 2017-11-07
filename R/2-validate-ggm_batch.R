e21964ae082d1e58d89fdc5f9f535c75ca1e4e72
Johann Hawe <johann.hawe@helmholtz-muenchen.de>
2017-11-07 16:00:29 +0100

    reverted back to validate all sentinels in validation batch script

--- R/2-validate-ggm_batch.R
+++ R/2-validate-ggm_batch.R
@@ -2,7 +2,7 @@
 # parses the sentinels from the available data files
 load("results/current/data.processed.RData")
 
-for(sentinel in c("rs3809627","rs17850402","rs12412214","rs11793438")) { #names(data)){
+for(sentinel in names(data)) {
   cmd <- paste0("qsub -cwd -V -q long_fed25 -pe smp 1 -hard -l job_mem=4G -b y ")
   cmd <- paste0(cmd, "-N ", sentinel, " ",
                 "-o results/current/", sentinel, ".validation.out ", 

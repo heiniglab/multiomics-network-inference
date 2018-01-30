040813f3d89a5c8569d3f828a8d04d00565bd1f8
Johann Hawe <johann.hawe@helmholtz-muenchen.de>
2018-01-30 09:52:02 +0100

    Fixed bug where no_prior simulation fit was not written to output file. Added additional plots for simulation results.

--- rework/R/apply-ggm-simulation.R
+++ rework/R/apply-ggm-simulation.R
@@ -7,23 +7,22 @@ library(GenomicRanges)
 
 source("R/lib.R")
 source("R/priors.R")
+source("R/bdgraph-supplement.R")
 
 ifile <- snakemake@input[[1]]
 ofile <- snakemake@output[[1]]
 cores <- snakemake@threads
 plotdir <- snakemake@params$plotdir
+iter <- snakemake@params$iter
+burnin <- snakemake@params$butnin
 
 print(paste0("Processing file: ", ifile, 
              " using ", cores, " cores."))
 
 # load data
 load(ifile)
-
-# set some parameters
-iter <- 50000
-burnin <- 10000
-
 d <- data.sim$data
+
 # sentinel name to generate plot filename
 s <- colnames(d)[grepl("^rs", colnames(d))]
 
@@ -38,14 +37,14 @@ ggm_fit <- bdgraph(d, method = "gcgm",
 
 ggm_fit_no_priors <- bdgraph(d, method = "gcgm",
                              iter = iter, burnin = burnin, 
-                             g.prior = priors, g.start = gstart, 
+                             g.start = gstart, 
                              save.all=T, cores=cores)
 
 # plot the bdgraph summary files
 pdf(paste0(plotdir, s, "-simulation-ggmsummary.pdf"))
-traceplot(ggm_fit)
+temp <- summary(ggm_fit)
 plotcoda(ggm_fit)
-traceplot(ggm_fit_no_priors)
+temp <- summary(ggm_fit_no_priors)
 plotcoda(ggm_fit_no_priors)
 dev.off()
 
@@ -53,6 +52,9 @@ dev.off()
 ggm_graph <- graph.from.fit(ggm_fit, ranges)
 ggm_graph_no_priors <- graph.from.fit(ggm_fit_no_priors, ranges)
 print(paste0("Saving results to ", ofile))
-save(file=ofile, ggm_fit, ranges, data.sim, ggm_graph, ggm_graph_no_priors)
+save(file=ofile, 
+     ggm_fit, ggm_fit_no_priors, 
+     ranges, data.sim, 
+     ggm_graph, ggm_graph_no_priors)
 
 sink()

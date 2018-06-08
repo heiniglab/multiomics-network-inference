0730c6ca7e089c2b3e58f501603e1777d88e0cb7
Johann Hawe <johann.hawe@helmholtz-muenchen.de>
2018-06-08 14:33:00 +0200

    Refractored shortest path part of range collecting.

--- scripts/collect-ranges.R
+++ scripts/collect-ranges.R
@@ -7,7 +7,9 @@
 #' @date 2017/03/04
 #'
 
-# load packages ----------------------------------------------------------------
+# ------------------------------------------------------------------------------
+# load packages, source scripts
+# ------------------------------------------------------------------------------
 library(GenomicRanges)
 library(GenomicFeatures)
 library(FDb.InfiniumMethylation.hg19)
@@ -18,8 +20,11 @@ library(graph)
 library(RBGL) # for shortest paths
 library(Matrix)
 
-# source common scripts --------------------------------------------------------
 source("scripts/lib.R")
+
+# ------------------------------------------------------------------------------
+# Define needed methods
+# ------------------------------------------------------------------------------
 
 #' Gets the shortest paths between two sets of genes
 #'
@@ -29,17 +34,20 @@ source("scripts/lib.R")
 #' @param cis List of nodes
 #' @param trans List of nodes
 #' @param string_db Instance of the string database to be used (graphNEL)
+#' 
 #' @return A vector of gene symbols being on the shortest path between the two lists
 #' of genes as found in the validated string network
 #'
 #' @author Johann Hawe, Matthias Heinig
 #' 
-get.string.shortest.paths <- function(cis, trans, snp_genes, string_db) {
+get_string_shortest_paths <- function(cis, trans, snp_genes, string_db) {
   
   g <- string_db
-  nodes <- nodes(string_db)
+  nodes <- nodes(g)
   
+  # ----------------------------------------------------------------------------
   # ensure to have only nodes in our giant cluster
+  # ----------------------------------------------------------------------------
   cis <- cis[which(cis %in% nodes)]
   trans <- trans[which(trans %in% nodes)]
   snp_genes <- snp_genes[which(snp_genes %in% nodes)]
@@ -47,48 +55,61 @@ get.string.shortest.paths <- function(cis, trans, snp_genes, string_db) {
     return(NULL)
   }
   
+  # ----------------------------------------------------------------------------
   # calculate weights for nodes
+  print("Starting propagation.")
+  # ----------------------------------------------------------------------------
   prop = propagation(graph2sparseMatrix(g), n.eigs=500, 
                      from=cis, to=trans, sum="both")
   node_weights <- rowSums(prop)
-  
-  # get the best trans gene
-  best_trans = snp_genes[which.max(prop[snp_genes,"from"])]
-  cat("best trans genes: ", best_trans, "\n")
-  
   ## find the shortest path with maximal weight
   ## we have an algorithm that finds minimum node weight paths so we need
   ## to turn the weighting around
   node_weights = max(node_weights) - node_weights + 1
   
+  # get the best trans gene as target for SP calculation
+  best_trans = snp_genes[which.max(prop[snp_genes,"from"])]
+  print(paste0("Best trans genes: ", paste(best_trans, collapse=", ")))
+  
+  # ----------------------------------------------------------------------------
   # extract shortest paths
+  print("Extracting shortest paths to best trans genes.")
+  # ----------------------------------------------------------------------------
   sp = min.node.weight.path(g, node_weights, from=cis, to=best_trans)
   nodes <- setdiff(unlist(lapply(sp, "[", "path_detail")), NA)
   nodes <- setdiff(nodes, c(trans, cis))
+  
   return(nodes)
 }
 
-#' Gets ranges-data for a specific sentinel SNP
+#' Gets all ranges-data for a specific sentinel SNP
 #'
-#' Check for each sentinel SNP which genes are near it and its associated CpGs and gets their ranges.
-#' Also saves the ranges of the sentinel and its other associated SNPs.
-#' Filters by number of CpGs per sentinel.
+#' Check for each sentinel SNP which genes are near it and its associated 
+#' CpGs and gets their ranges. Also saves the ranges of the sentinel 
+#' and its other associated SNPs. Filters by number of CpGs per sentinel.
 #'
 #' @param id The id of sentinel to be processed.
 #' @param cosmo The individual meqtl associations
 #' @param trans_meQTL The pruned trans-meQTL table
 #' 
 #' @value A list containing a relevant ranges for the sentinel snp:
-#'  cpg ranges -> cpgs
-#'  snp ranges -> snp.ranges
-#'  sentinel ranges -> sentinel.range and sentinel.ext.range (extended by window)
-#'  gene ranges -> cpg.genes and snp.genes
-#' 
+#'  cpg ranges (meQTL cpgs for sentinel) -> cpgs
+#'  snp ranges -> snp_ranges
+#'  sentinel ranges -> sentinel_range and sentinel_ext_range
+#'  cpg and snp gene ranges -> cpg_genes and snp_genes
+#'  tfs -> TFs with TFBS at CpGs
+#'  spath -> genes on shortest path between CpGs and snp genes
+#'  
 #' @author Johann Hawe
 #' 
 #' @date 02/07/2017
 #'
-collect.ranges <- function(id, cosmo, trans_meQTL, fcpgcontext, sentinel_only=T) {
+collect_ranges <- function(id, cosmo, trans_meQTL, 
+                           fcpgcontext, sentinel_only=T) {
+  
+  # ----------------------------------------------------------------------------
+  print("collecting SNP and CpG ranges.")
+  # ----------------------------------------------------------------------------
   
   pairs = which(trans_meQTL[,"sentinel.snp"] == id)
   
@@ -133,6 +154,10 @@ collect.ranges <- function(id, cosmo, trans_meQTL, fcpgcontext, sentinel_only=T)
   sroi <- sentinel_extrange
   names(sroi) <- id
   
+  # ----------------------------------------------------------------------------
+  print("Retrieving SNP and CpG genes.")
+  # ----------------------------------------------------------------------------
+  
   # for now use the results from the enrichment analysis to get the
   # relevant SNP genes
   genes_sroi <- gene_annot[gene_annot %over% sroi]
@@ -167,44 +192,48 @@ collect.ranges <- function(id, cosmo, trans_meQTL, fcpgcontext, sentinel_only=T)
     return(NULL)
   }
   
+  # ----------------------------------------------------------------------------
+  print("Collecting TFs and shortest path genes.")
+  # ----------------------------------------------------------------------------
+  
   tfs <- NULL
   sp <- NULL
   
-  ## now get also the SYMBOLS of the genes being on the shortest paths
-  ## between any snp<->cpg gene combinations
-  cat("Collecting shortest paths.\n")
-  
+  # get cpg ids and SNP gene symbols
   cpgs <- names(croi)
   snp_genes <- unique(genes_sroi$SYMBOL)
   
-  # modify string.db to contain our CpGs  
+  # modify string_db to contain our CpGs  
   
   # load the cpg-tf context
   tfbs_ann <- get.chipseq.context(names(croi), fcpgcontext)
   cpgs_with_tfbs <- cpgs[cpgs %in% rownames(tfbs_ann[rowSums(tfbs_ann)>0,])]
   snp_genes_in_string <- snp_genes[snp_genes %in% nodes(string_db)]
   
-  string_db <- add.to.graphs(list(string_db), id, snp_genes, 
+  # get locus graph
+  locus_graph <- add.to.graphs(list(string_db), id, snp_genes, 
                              cpgs_with_tfbs, tfbs_ann)[[1]]
   
   # get tfs connected to cpgs
-  tfs = unique(unlist(adj(string_db, cpgs_with_tfbs)))
-  cat("Annotated TFs: ", tfs, "\n")
+  tfs = unique(unlist(adj(locus_graph, cpgs_with_tfbs)))
+  print(paste0("Annotated TFs: ", paste(tfs, collapse=", ")))
+  
   if(length(tfs)<1){
     warning("No TFs, skipping shortest paths calculation.")
   } else {
     # the nodes we want to keep
-    nodeset = c(nodes(string_db), setdiff(tfs, "KAP1"), 
+    nodeset <- c(nodes(string_db), setdiff(tfs, "KAP1"), 
                 snp_genes_in_string, cpgs_with_tfbs)
-    string_db = subGraph(intersect(nodes(string_db), nodeset), string_db)
+    locus_graph <- subGraph(intersect(nodes(locus_graph), nodeset), locus_graph)
     
-    syms_sp <- get.string.shortest.paths(cis = cpgs_with_tfbs, 
+    print("Finding shortest path genes.")
+    syms_sp <- get_string_shortest_paths(cis = cpgs_with_tfbs, 
                                          trans=unique(c(snp_genes_in_string,
                                                         tfs)), 
                                          snp_genes=snp_genes_in_string,
-                                         string_db)
+                                         locus_graph)
     
-    cat("Shortest paths genes: ", paste0(syms_sp, sep=";"))
+    print(paste0("Shortest paths genes: ", paste(syms_sp, collapse=", ")))
     
     if(length(syms_sp) < 1){
       warning("No shortest path genes.")
@@ -219,7 +248,8 @@ collect.ranges <- function(id, cosmo, trans_meQTL, fcpgcontext, sentinel_only=T)
   # construct our result list
   result <-  list(cpgs=croi,sentinel_range=sentinel_range,
                   sentinel_ext_range=sentinel_extrange, 
-                  snp_genes=genes_sroi, cpg_genes=genes_croi)
+                  snp_genes=genes_sroi, cpg_genes=genes_croi,
+                  cpg_genes_by_cpg=genes_croi_by_cpg)
   
   if(!sentinel_only) {
     result$snp_ranges <- snp_ranges
@@ -233,18 +263,26 @@ collect.ranges <- function(id, cosmo, trans_meQTL, fcpgcontext, sentinel_only=T)
   return(result)
 }
 
-# Processing start -------------------------------------------------------------
+# ------------------------------------------------------------------------------
+# Start processing
+# ------------------------------------------------------------------------------
 
-# get paths
+# ------------------------------------------------------------------------------
+# Get snakemake params
+# ------------------------------------------------------------------------------
 fcosmo <- snakemake@input[["tcosmo"]]
 fmeqtl <- snakemake@input[["meqtl"]]
 fstring <- snakemake@input[["string"]]
-fcpgcontext <- snakemake@input[["cpgcontext"]]
-ofile <- snakemake@output[[1]]
 
-# get params
+# TODO: create this file from scratch!
+fcpgcontext <- snakemake@input[["cpgcontext"]]
+
+ofile <- snakemake@output[[1]]
 sentinel <- snakemake@params[["sentinel"]]
-window <- snakemake@params[["window"]]
+
+# ------------------------------------------------------------------------------
+# Load and preprocess data
+# ------------------------------------------------------------------------------
 
 cat("Loading data.\n")
 
@@ -254,10 +292,17 @@ string_db <- readRDS(fstring)
 # load trans-meQTL table
 trans_meQTL = read.csv(fmeqtl, 
                        sep="\t", stringsAsFactors=F)
-# load trans-cosmo
+
+# load trans-cosmo information
 cosmo <- readRDS(fcosmo)
+
+# ------------------------------------------------------------------------------
+# Collect and save ranges
+# ------------------------------------------------------------------------------
+
+print("Collecting ranges.")
 
 # collect all associated ranges
 ranges <- collect.ranges(sentinel, cosmo, trans_meQTL, fcpgcontext)
-saveRDS(file=ofile, ranges)
 
+saveRDS(file=ofile, ranges)

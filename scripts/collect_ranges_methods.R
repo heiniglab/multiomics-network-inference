ec280474e4e2b3fe6238c033f271a0a279293dc8
Johann Hawe <johann.hawe@jhu.edu>
2019-01-28 20:08:50 -0500

    Fixed issue with tfs_by_transgenes

--- scripts/collect_ranges_methods.R
+++ scripts/collect_ranges_methods.R
@@ -70,6 +70,7 @@ get_shortest_paths <- function(cis, trans, snp_genes, ppi_db, best_trans=NULL) {
 #' ------------------------------------------------------------------------------
 get_tfs_by_transGene <- function(tfbs, trans_genes, gene_annot) {
   # get TFs and map their corresponding trans gene
+  trans_genes <- trans_genes[trans_genes$SYMBOL %in% rownames(tfbs)]
   tfs_by_transGene <- c()
   for(i in 1:length(trans_genes)){
     s <- trans_genes[i]$SYMBOL
@@ -118,4 +119,36 @@ collect_shortest_path_genes <- function(tfs, trans_genes, tfs_by_transGene,
     sp <- gene_annot[gene_annot$SYMBOL %in% syms_sp]
     return(sp)
   }
+}
+
+# ------------------------------------------------------------------------------
+# Simply helper to quickly plot ranges
+# ------------------------------------------------------------------------------
+plot_ranges <- function(ranges, fout) {
+  require(ggplot2)
+  cols <- set_defaultcolors()
+  sfm <- scale_fill_manual(values=cols)
+  
+  # get number of entities
+  sg <- length(ranges$snp_genes)
+  tfs <- length(ranges$tfs)
+  sp <- length(ranges$spath)
+  trans_assoc <- length(ranges$trans_genes)
+
+  # create data-frame for use with ggplot
+  toplot <- data.frame(name=c("SNP genes", "TFs", "shortest path",
+                       "trans associated"),
+                       count=c(sg, tfs, sp, trans_assoc))
+
+  gt <- ggtitle(paste0("Entities for sentinel ", names(ranges$sentinel), 
+                       " in tissue " , ranges$tissue, "."))
+
+  pdf(fout, width=8, height=8)
+  
+  # barplot of number of individual entity types
+  ggplot(aes(y=count, x=name), data=toplot) +
+    geom_bar(stat="identity", position="dodge") + gt +
+    theme(axis.text.x = element_text(angle = 90, hjust = 1))
+
+  dev.off()
 }

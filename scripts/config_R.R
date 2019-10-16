ee6ad5fdbc772b9a89e0ef724eb3b874c357dd8e
Johann Hawe <johann.hawe@helmholtz-muenchen.de>
2019-10-16 08:23:51 +0200

    Re-added inference packages

--- scripts/config_R.R
+++ scripts/config_R.R
@@ -1,5 +1,8 @@
 print("Installing libraries to:")
 .libPaths()
 
+source("https://bioconductor.org/biocLite.R")
+biocLite(c("GeneNet", "GENIE3", "glasso", "BDgraph"))
+
 install.packages("packages/iRafNet_1.1-2.tar.gz", repos=NULL, type="source")
 #install.packages("packages/R_peer_source_1.3.tgz", repos=NULL, type="source", INSTALL_opts=c("--no-multiarch"))

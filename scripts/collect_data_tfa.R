dae6bcbf2f9a9d052ca89c6f3e60f7a0dd9ad6f1
Johann Hawe <johann.hawe@helmholtz-muenchen.de>
2019-11-07 13:34:56 +0100

    Fixed script to support both TFA and EXPR data collection

--- scripts/collect_data_tfa.R
+++ scripts/collect_data_tfa.R
@@ -20,7 +20,6 @@ print("Load libraries, source scripts.")
 # ------------------------------------------------------------------------------
 suppressPackageStartupMessages(library(GenomicRanges))
 source("scripts/lib.R")
-source("scripts/collect_data_methods_tfa.R")
 
 # ------------------------------------------------------------------------------
 print("Get snakemake params.")
@@ -39,6 +38,16 @@ fceqtl <- snakemake@input[["ceqtl"]]
 cohort <- snakemake@wildcards$cohort
 sentinel <- snakemake@wildcards$sentinel
 seed <- snakemake@wildcards$seed
+tfa_or_expr <- snakemake@params$tfa_or_expr
+
+print(paste0("Using '", tfa_or_expr, "' for gene measurements"))
+      
+# we source a different methods script in case we get TFA
+if("tfa" %in% tfa_or_expr) {
+  source("scripts/collect_data_methods_tfa.R")
+} else {
+  source("scripts/collect_data_methods.R")
+}
 
 # output
 fout <- snakemake@output[[1]]
@@ -50,12 +59,24 @@ print(paste0("Sentinel is ", sentinel, "."))
 print("Prepare probe and snp ids.")
 # ------------------------------------------------------------------------------
 ranges <- readRDS(franges)
+
+# get expression probes
+expr_probes = c(unlist(ranges$snp_genes$ids),
+                unlist(ranges$cpg_genes$ids),
+                unlist(ranges$tfs$ids),
+                unlist(ranges$spath$ids),
+                unlist(ranges$trans_genes$ids))
+expr_probes <- unique(expr_probes)
+
+# get expr gene symbols (for TFA based data)
 expr_syms = c(unlist(ranges$snp_genes$SYMBOL),
                 unlist(ranges$cpg_genes$SYMBOL),
                 unlist(ranges$tfs$SYMBOL),
                 unlist(ranges$spath$SYMBOL),
                 unlist(ranges$trans_genes$SYMBOL))
 expr_syms <- unique(expr_syms)
+
+# get methylation probes
 meth_probes <- names(ranges$cpgs)
 
 # ------------------------------------------------------------------------------
@@ -63,10 +84,14 @@ print("Loading cohort data.")
 # ------------------------------------------------------------------------------
 if("kora" %in% cohort) {
   load(fkora_data)
-  acts <- readRDS(fkora_activities)
+  if("tfa" %in% tfa_or_expr) {
+    acts <- readRDS(fkora_activities)
+  }
 } else if ("lolipop" %in% cohort) {
   load(flolipop_data)
-  acts <- readRDS(flolipop_activities)
+  if("tfa" %in% tfa_or_expr) {
+    acts <- readRDS(flolipop_activities)
+  }
 } else {
   stop("Cohort not supported.")
 }
@@ -74,25 +99,32 @@ if("kora" %in% cohort) {
 # ------------------------------------------------------------------------------
 print("Create merged data frame.")
 # ------------------------------------------------------------------------------
-genes <- colnames(acts)[colnames(acts) %in% expr_syms]
-data <- cbind.data.frame(covars,
-                         acts[,genes, drop=F],
-                         meth[,colnames(meth) %in% meth_probes, drop=F],
-                         geno[,colnames(geno) %in% sentinel, drop=F],
-                         geno_ids=rownames(geno),
-                         stringsAsFactors=F)
+if("tfa" %in% tfa_or_expr) {
+  print("Collecting TFA based data.")
+  genes <- colnames(acts)[colnames(acts) %in% expr_syms]
+  data <- cbind.data.frame(covars,
+                           acts[,genes, drop=F],
+                           meth[,colnames(meth) %in% meth_probes, drop=F],
+                           geno[,colnames(geno) %in% sentinel, drop=F],
+                           geno_ids=rownames(geno),
+                           stringsAsFactors=F)
+} else {
+  print("Collecting EXPR based data.")
+  data <- cbind.data.frame(covars,
+                           expr[,colnames(expr) %in% expr_probes, drop=F],
+                           meth[,colnames(meth) %in% meth_probes, drop=F],
+                           geno[,colnames(geno) %in% sentinel, drop=F],
+                           geno_ids=rownames(geno),
+                           stringsAsFactors=F)
+}
 
 # ------------------------------------------------------------------------------
-print("Save raw data.")
+print("Saving raw data.")
 # ------------------------------------------------------------------------------
 saveRDS(file=fout_raw, data)
 
-# check which probes we didnt have available
-print("Unavailable probes:")
-print(meth_probes[!meth_probes %in% colnames(meth)])
-
 # remove not needed remaining data frames
-rm(expr, meth, covars, acts)
+rm(expr, meth, covars)
 gc()
 
 print(paste0("Data dimensions: ", paste(dim(data), collapse=",")))
@@ -100,7 +132,11 @@ print(paste0("Data dimensions: ", paste(dim(data), collapse=",")))
 # ------------------------------------------------------------------------------
 print("Removing covariate effects from raw data.")
 # ------------------------------------------------------------------------------
-data <- adjust_data(sentinel, ranges, genes, data, geno, fccosmo, fceqtl)
+if("tfa" %in% tfa_or_expr) {
+  data <- adjust_data(sentinel, ranges, genes, data, geno, fccosmo, fceqtl)  
+} else {
+  data <- adjust_data(sentinel, ranges, data, geno, fccosmo, fceqtl)
+}
 
 # ------------------------------------------------------------------------------
 print("Saving adjusted data.")

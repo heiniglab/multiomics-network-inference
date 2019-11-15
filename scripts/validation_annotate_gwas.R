85252d77e95f94fdbfc40707b92efe716b8337f2
Johann Hawe <johann.hawe@helmholtz-muenchen.de>
2019-11-15 15:58:15 +0100

    Added GWAS trait check for genes. Still have to check on overlap with snp trait.

--- scripts/validation_annotate_gwas.R
+++ scripts/validation_annotate_gwas.R
@@ -16,6 +16,7 @@ source("scripts/snipe.R")
 
 fvalidation <- snakemake@input$validation
 fgwas <- snakemake@input$gwas
+feqtlgen <- snakemake@input$eqtlgen
 
 fout_validation <- snakemake@output$validation
 
@@ -43,6 +44,36 @@ val$gwas_disease_trait <- NA
 val$gwas_disease_trait <- unlist(traits_per_snp[match(val$sentinel,
                                                 names(traits_per_snp))])
 
+# also process the gene list: get cis-eQTL snps and check their GWAS annot
+print("Annotating selected genes with eQTL SNP GWAS traits.")
+get_eqtl_snps <- function(gene_list, eqtl) {
+  eqtl %>% filter(GeneSymbol %in% gene_list) %>%
+    pull(SNP) %>% unique
+}
+print("Loading eQTLgen data.")
+eqtlgen <- read_tsv(feqtlgen)
+
+print("Annotating...")
+traits_per_locus_genes <- mclapply(val$non_snp_genes_selected.list, function(genes) {
+  if(!is.na(genes)) {
+    gene_list <- strsplit(genes, ";")[[1]]
+    results <- sapply(gene_list, function(g) {
+      snps <- get_eqtl_snps(g, eqtlgen)  
+      
+      # only keep rs-ids, otherwise SNiPA will not work
+      snps <- snps[grepl("^rs", snps)]
+      get_gwas_traits(snps, gwas)
+    })
+    paste0(gene_list, ":", results, collapse = ";")
+    
+  } else {
+    NA
+  }
+}, mc.cores=threads)
+print("Done.")
+val$non_snp_genes_selected.gwas_traits <- unlist(traits_per_locus_genes)
+
+sapply(val$gwas)
 # ------------------------------------------------------------------------------
 print("Writing output file.")
 # ------------------------------------------------------------------------------

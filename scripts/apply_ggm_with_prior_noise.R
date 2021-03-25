0942008e64f30420564fe628ca4ff2a97f31b34b
Johann Hawe <johann.hawe@helmholtz-muenchen.de>
2021-03-25 12:27:44 +0100

    Implements simple prior noies for replication analysis.

---
+++ scripts/apply_ggm_with_prior_noise.R
@@ -0,0 +1,121 @@
+#!/usr/bin/env Rscript
+# ------------------------------------------------------------------------------
+#' Apply graphical model inference on data collected for a specific sentinel.
+#' Adds a certain amount of noise to the prior information prior to inference.
+#'
+#' @author Johann Hawe <johann.hawe@tum.de>
+# ------------------------------------------------------------------------------
+
+log <- file(snakemake@log[[1]], open="wt")
+sink(log)
+sink(log, type="message")
+
+# ------------------------------------------------------------------------------
+print("Prepare libraries and source scripts.")
+# ------------------------------------------------------------------------------
+library(pheatmap)
+suppressPackageStartupMessages(library(GenomicRanges))
+library(igraph)
+library(graph)
+library(reshape2)
+source("scripts/lib.R")
+source("scripts/reg_net.R")
+
+# ------------------------------------------------------------------------------
+print("Get snakemake parameters.")
+# ------------------------------------------------------------------------------
+
+#input
+franges <- snakemake@input[["ranges"]]
+fdata <- snakemake@input[["data"]]
+fppi_db <- snakemake@input[["ppi_db"]]
+fpriors <- snakemake@input[["priors"]]
+fcpg_context <- snakemake@input[["cpg_context"]]
+ftss_context <- snakemake@input[["tss_context"]]
+
+# output
+fout <- snakemake@output$fit
+fsummary_plot <- snakemake@output$summary_file
+
+# params
+threads <- snakemake@threads
+prior_noise <- snakemake@prior_noise
+
+# ------------------------------------------------------------------------------
+print("Load and prepare data.")
+# ------------------------------------------------------------------------------
+data <- readRDS(fdata)
+
+# remove (rare) all-NA cases. This can happen due to scaling of all-zero entities,
+# which can arise due to a very large number of cis-meQTLs which effects get
+# removed from the CpGs during data preprocessing.
+# NOTE: we could possibly handle this differently? Seems that these particular
+# cpgs are highly influenced by genetic effects?
+use <- apply(data,2,function(x) (sum(is.na(x)) / length(x)) < 1)
+data <- data[,use]
+
+print("Dimensions of data:")
+print(dim(data))
+
+priors <- readRDS(fpriors)
+
+# filter for available data in priros
+priors <- priors[colnames(data), colnames(data)]
+ranges <- readRDS(franges)
+
+# load PPI DB
+ppi_db <- readRDS(fppi_db)
+
+if(ranges$seed == "meqtl") {
+  fcontext <- fcpg_context
+} else {
+  fcontext <- ftss_context
+}
+
+# ------------------------------------------------------------------------------
+# for catching the genenet summary plots, we open the pdf connection here
+# ------------------------------------------------------------------------------
+pdf(fsummary_plot)
+
+# ------------------------------------------------------------------------------
+# We add different levels of noise and infer graphs for all scenarios
+print("Infer regulatory networks.")
+# ------------------------------------------------------------------------------
+
+# helper to get a noisy prior matrix
+noisify_priors <- function(priors, noise_level) {
+  PSEUDO_PRIOR <- 1e-7
+  number_edges_with_prior <- sum(priors>PSEUDO_PRIOR) / 2
+  number_entries_to_switch <- round(noise_level * number_edges_with_prior)
+  
+  prior_idx <- sample(which(upper.tri(priors) & priors > PSEUDO_PRIOR), 
+                      number_entries_to_switch)
+  non_prior_idx <- sample(which(upper.tri(priors) & priors == PSEUDO_PRIOR),
+                         number_entries_to_switch)
+  
+  # swap idxs
+  temp <- priors[prior_idx]
+  priors[prior_idx] <- priors[non_prior_idx]
+  priors[non_prior_idx] <- temp
+  
+  return(priors)
+}
+
+noise_levels <- c(0.1)
+result <- lapply(noise_levels, function(noise_level) {
+  priors <- noisify_priors(priors, noise_level)
+  infer_all_graphs(data, priors, ranges, fcontext, ppi_db,
+                   threads)
+})
+names(result) <- paste0("noise_level_", noise_levels)
+dev.off()
+
+# ------------------------------------------------------------------------------
+print("All done. Saving results.")
+# ------------------------------------------------------------------------------
+saveRDS(file=fout, result)
+
+# ------------------------------------------------------------------------------
+print("Session info:")
+# ------------------------------------------------------------------------------
+sessionInfo()

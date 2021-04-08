ef8865f68ee4143b52ac39b7d02b88c5c07c6631
Johann Hawe <johann.hawe@helmholtz-muenchen.de>
2021-04-08 15:29:31 +0200

    Adds SLURM submission script for benchmarking

---
+++ submit_slurm_benchmark.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# default call for SLURM submit with snakemake
+target=$1
+log=$2
+
+if [[ -z ${log} ]] ; then
+  log=$(basename ${target}).out
+fi
+
+# submit using snakemake -> check for command
+command -v snakemake >/dev/null 2>&1 || { echo "snakemake not available, exiting." ; exit 1 ; }
+
+# actual call
+nohup nice snakemake -u configs/slurm.json --jobs=200 -k --local-cores=1 \
+    --latency-wait 40 --cluster "sbatch --nice=10000 -w icb-neu-001 -t {cluster.time} -c {cluster.cpu} --mem-per-cpu {cluster.mem} \
+    -p {cluster.partition}" \
+    ${target} &> ${log} &
+
+echo "Running snakemake. Log-file: ${log}"

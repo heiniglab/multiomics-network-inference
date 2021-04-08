6d465753e99d2160d19fe7c749a4cc47f573c00d
Johann Hawe <johann.hawe@helmholtz-muenchen.de>
2021-04-08 15:30:35 +0200

    Removes necessity of giving parameters

--- submit_slurm_benchmark.sh
+++ submit_slurm_benchmark.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
 # default call for SLURM submit with snakemake
-target=$1
-log=$2
-
-if [[ -z ${log} ]] ; then
-  log=$(basename ${target}).out
-fi
+target=gather_benchmark_results
+log=gather_benchmark_results.log
 
 # submit using snakemake -> check for command
 command -v snakemake >/dev/null 2>&1 || { echo "snakemake not available, exiting." ; exit 1 ; }

f5857c534b4ea50e34e035ebf190a57f158f3b74
Johann Hawe <johann.hawe@helmholtz-muenchen.de>
2021-04-27 11:42:15 +0200

    Changes to use profile for benchmarking

--- submit_slurm_benchmark.sh
+++ submit_slurm_benchmark.sh
@@ -8,9 +8,7 @@ log=gather_benchmark_results.log
 command -v snakemake >/dev/null 2>&1 || { echo "snakemake not available, exiting." ; exit 1 ; }
 
 # actual call
-nohup nice snakemake -u configs/slurm.json --jobs=200 -k --local-cores=1 \
-    --latency-wait 40 --cluster "sbatch --nice=10000 -w icb-neu-001 -t {cluster.time} -c {cluster.cpu} --mem-per-cpu {cluster.mem} \
-    -p {cluster.partition}" \
+nohup nice snakemake --profile ./profiles/slurm_singlenode \
     ${target} &> ${log} &
 
 echo "Running snakemake. Log-file: ${log}"

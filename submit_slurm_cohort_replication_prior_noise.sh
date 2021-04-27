4a3d73acb4741ff89858643c5d78f8eafe18834f
Johann Hawe <johann.hawe@helmholtz-muenchen.de>
2021-04-27 13:37:10 +0200

    Adds submit script for replication analysis with prior noise

---
+++ submit_slurm_cohort_replication_prior_noise.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# default call for SLURM submit with snakemake
+# targetting the cohort study
+
+echo "Checking for snakemake ------------------------------"
+
+command -v snakemake >/dev/null 2>&1 || { echo "snakemake not available, exiting." ; exit 1 ; }
+
+
+echo "Executing snakemake ---------------------------------"
+
+target=apply_ggm_noisy_priors
+logFile=${target}.log
+nohup nice snakemake --profile=./profiles/slurm \
+  ${target} &> ${logFile} &
+
+echo "Snakemake running -----------------------------------"
+echo "Log-file: ${logFile}"

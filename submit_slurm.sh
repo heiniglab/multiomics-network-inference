390b5c336f416bf56217ef521e08bf285319228b
Johann Hawe <johann.hawe@helmholtz-muenchen.de>
2021-10-18 15:54:07 +0200

    Updated basic SLURM submission script

--- submit_slurm.sh
+++ submit_slurm.sh
@@ -12,9 +12,7 @@ fi
 command -v snakemake >/dev/null 2>&1 || { echo "snakemake not available, exiting." ; exit 1 ; }
 
 # actual call
-nohup nice snakemake -u configs/slurm.json --jobs=200 -k --local-cores=1 \
-    --latency-wait 40 --cluster "sbatch --nice=10000 -t {cluster.time} -c {cluster.cpu} --mem-per-cpu {cluster.mem} \
-    -p {cluster.partition}" \
+nohup nice snakemake --profile ./profiles/slurm \
     ${target} &> ${log} &
 
 echo "Running snakemake. Log-file: ${log}"
